4b20fd48003bc16f3a729e828843437dd1fd9dc8
JaxLordOrder <singularityoder@gmail.com>
2026-09-02 22:36:15 -0300

    Add race definitions and permissions in Tabela.lua

---
+++ Tabela.lua
@@ -0,0 +1,92 @@
+module.AllRaces = {
+	2267845183,
+	9592003273,
+}
+
+module.Races = {
+	-- Raças com acesso restrito (apenas devs)
+	["Anjo"] = {
+		PlayersPermission = {"Nickname5"},
+		Icon = 81599403581004,
+		RaceName = "anjo",
+		RaceTag = "anjo",
+	},
+	["VastoLorde"] = {
+		PlayersPermission = {"Nickname5"},
+		Icon = 107557120350668,
+		RaceName = "VastoLorde",
+		RaceTag = "vastolorde",
+	},
+	["Majin"] = {
+		PlayersPermission = {"NickName5"},
+		Icon = 121525410617389,
+		RaceName = "majin",
+		RaceTag = "majin",
+	},
+	["Hakaishin"] = {
+		PlayersPermission = {"NickName5"},
+		Icon = 75841837094533,
+		RaceName = "Hakaishin",
+		RaceTag = "hakai",
+	},
+	["KaioShin"] = {
+		PlayersPermission = {"NickName5"},
+		Icon = 88719027239897,
+		RaceName = "KaioShin",
+		RaceTag = "kaioshin",
+	},
+	["Mahoraga"] = {
+		PlayersPermission = {"NickName5"},
+		Icon = 126275912972530,
+		RaceName = "mahoraga",
+		RaceTag = "mahoraga",
+	},
+	["Kilua"] = {
+		PlayersPermission = {"Nickname5"},
+		Icon = 18367318475,
+		RaceName = "kilua",
+		RaceTag = "kilua",
+	},
+	["Uchiha"] = {
+		PlayersPermission = {"Nickname5"},
+		Icon = 6807731616,
+		RaceName = "Uchiha",
+		RaceTag = "uchiha",
+	},
+	["Instinto"] = {
+		PlayersPermission = {"Nickname5"},
+		Icon = 10501016585,
+		RaceName = "Instinto",
+		RaceTag = "Instinto",
+	},
+	["Cosmic"] = {
+		PlayersPermission = {"NickName5"},
+		Icon = 11229028251,
+		RaceName = "Cosmic",
+		RaceTag = "cosmic",
+	},
+	["Xeno"] = {
+		PlayersPermission = {"NickName5"},
+		Icon = 16708672510,
+		RaceName = "Xeno",
+		RaceTag = "Xeno",
+	},
+
+ -- racas em teste
+	["Evil Morty"] = {
+		PlayersPermission = {"Nickname5"},
+		Icon = 8418743242,
+		RaceName = "EvilMorty",
+		RaceTag = "EvilMorty",
+	},
+		["Anão"] = {
+		PlayersPermission = {"NickName5"},
+		Icon = 130920965488753,
+		RaceName = "Anão",
+		RaceTag = "Anão",
+	},
+
+	
+}
+
+return module
