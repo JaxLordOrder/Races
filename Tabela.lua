252e36f1489283add366ef9b935d29e3f2bd7fd4
JaxLordOrder <singularityoder@gmail.com>
2026-09-03 22:28:56 -0300

    Add player 'luccaragnar' to Anjo permissions

--- Tabela.lua
+++ Tabela.lua
@@ -6,7 +6,7 @@ module.AllRaces = {
 module.Races = {
 	-- Raças com acesso restrito (apenas devs)
 	["Anjo"] = {
-		PlayersPermission = {"157_lufi"},
+		PlayersPermission = {"157_lufi","luccaragnar"},
 		Icon = 81599403581004,
 		RaceName = "anjo",
 		RaceTag = "anjo",
